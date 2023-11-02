22c4da4e9771be86877662b6fdf6d7519037d850
Sarthak <sc2309>
2023-11-02 11:18:52 +0530

    diet page added

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from flask import Flask
 def myFirstApp():
     app = Flask(__name__)
     from views import views
+    from diet import diet
+    app.register_blueprint(diet, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
     return app
 
