362b6ef3bfd666e144f15a3556d51bc742abb043
Sarthak <sc2309>
2023-11-16 22:01:52 +0530

    completed feedback feature

--- coding_Competition-main/src/main.py
+++ coding_Competition-main/src/main.py
@@ -9,6 +9,8 @@ from signup import signup
 from diet import diet
 from routines import routines
 from signup2 import signup2
+from feedback import feedback
+from store_data import store_data
 
 def create_app():
     app = Flask(__name__)
@@ -22,6 +24,8 @@ def create_app():
     app.register_blueprint(diet, url_prefix='/')
     app.register_blueprint(routines, url_prefix='/')
     app.register_blueprint(signup2, url_prefix='/')
+    app.register_blueprint(store_data, url_prefix='/')
+    app.register_blueprint(feedback, url_prefix='/')
     return app
 
 if __name__ == "__main__":
