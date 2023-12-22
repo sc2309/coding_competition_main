8b46bb34103111677e1b2bb571fcb6d06b77de30
Sarthak <sc2309>
2023-12-22 17:41:12 +0530

    final commit

--- coding_Competition-main/src/Fitness.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-sports = Blueprint('fitness', __name__)
-
-@sports.route('/fitness')
-def index():
-    return render_template('fitness.html')
--- coding_Competition-main/src/feedback.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-feedback = Blueprint('feedback', __name__)
-
-@feedback.route('/feedback')
-def index():
-    return render_template('feedback.html')
